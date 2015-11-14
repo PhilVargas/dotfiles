b41292989fb9f1ec8425ad667a80699f107131e0
Phil Vargas <philipavargas@gmail.com>
2015-11-14 10:41:11 -0800

    refactor(shell.sh) remove node v0.10.38 as default
     - no long default node version to v0.10.38
    
     - reorder nvm/rvm calls. ruby yells if its not leading the $PATH

--- shell/env.sh
+++ shell/env.sh
@@ -3,7 +3,6 @@
 #-------------------------------------------------------------
 export PATH="/usr/local/bin:$PATH"
 export PATH="$HOME/.bin:$PATH"
-export PATH="$HOME/.nvm/v0.10.38/bin:$PATH" # Use nvm node over system node
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 #-------------------------------------------------------------
@@ -16,8 +15,9 @@ export LSCOLORS=exGxFxdxCxDxDxaccxaeex
 #-------------------------------------------------------------
 # Source scripts
 #-------------------------------------------------------------
+# set nvm source
+source $(brew --prefix nvm)/nvm.sh
+
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-# set nvm source
-source $(brew --prefix nvm)/nvm.sh
