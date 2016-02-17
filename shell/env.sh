024f118fac1aa375e874b63d9c18ba589ca28859
Phil Vargas <philipavargas@gmail.com>
2016-02-17 08:09:52 -0800

    bugfix(env.sh) change nvm source explicitly

--- shell/env.sh
+++ shell/env.sh
@@ -16,7 +16,7 @@ export LSCOLORS=exGxFxdxCxDxDxaccxaeex
 # Source scripts
 #-------------------------------------------------------------
 # set nvm source
-source $(brew --prefix nvm)/nvm.sh
+source ~/.nvm/nvm.sh
 
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
