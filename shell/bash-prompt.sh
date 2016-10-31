30b78a04cbc05828f1f1887dccd8c18f41142a9f
Phil Vargas <philipavargas@gmail.com>
2016-10-31 14:55:36 -0700

    bugfix(bash_prompt) update clean git status text

--- shell/bash-prompt.sh
+++ shell/bash-prompt.sh
@@ -12,7 +12,7 @@ DEFAULT="\033[0;00m"
 
 function git_color() {
   local git_status="$(git status 2> /dev/null)"
-  if [[ $git_status =~ "working directory clean" ]]; then
+  if [[ $git_status =~ "working tree clean" ]]; then
     if [[ $git_status =~ "Your branch is ahead of" ]]; then
       echo -ne $YELLOW
     else
